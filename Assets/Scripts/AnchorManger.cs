42a463bc4d55701913bb376ac0ebc809cad01f48
DESKTOP-4IHMTBP\akhil <g.saikiran@outlook.com>
2018-03-11 11:41:33 +0530

    added with experimental line renderer

--- Assets/Scripts/AnchorManger.cs
+++ Assets/Scripts/AnchorManger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleARCore;
+using UnityEngine.Events;
 
 public class AnchorManger : MonoBehaviour {
 
@@ -14,8 +15,37 @@ public class AnchorManger : MonoBehaviour {
 	public Transform point;
 	public Camera camera;
 	public GameObject currentobject;
+	//public ConnectLine latestLine;
+//	public GameObject line;
+	public UnityAction destroyline;
+
+	public AudioSource audio;
+
+	float currenttime = 0;
+	public static AnchorManger instance;
+
+	float accelerometerUpdateInterval = 1.0f / 60.0f;
+	// The greater the value of LowPassKernelWidthInSeconds, the slower the
+	// filtered value will converge towards current input sample (and vice versa).
+	float lowPassKernelWidthInSeconds = 1.0f;
+	// This next parameter is initialized to 2.0 per Apple's recommendation,
+	// or at least according to Brady! ;)
+	float shakeDetectionThreshold = 0.6f;
+
+	float lowPassFilterFactor;
+	Vector3 lowPassValue;
 
 
+	void Awake()
+	{
+		instance = this;
+		Input.multiTouchEnabled = false;
+
+		lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+		shakeDetectionThreshold *= shakeDetectionThreshold;
+		lowPassValue = Input.acceleration;
+		audio.volume = 0;
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -41,8 +71,14 @@ public class AnchorManger : MonoBehaviour {
 			//Debug.Log (anchor.transform.position);
 			if (anchor != null) {
 				currentobject = Instantiate (anchorPrefab, ppos, anchor.transform.rotation, anchor.transform);
+				//GameObject obj = Instantiate (line.gameObject, ppos, anchor.transform.rotation, anchor.transform);
+				//latestLine = obj.GetComponent<ConnectLine> ();
+				//destroyline += latestLine.EnbleGravity;
 			} else {
 				currentobject = Instantiate (anchorPrefab, ppos, Quaternion.identity, null);
+				//GameObject obj  = Instantiate (line.gameObject, ppos, Quaternion.identity, null);
+			//	latestLine = obj.GetComponent<ConnectLine> ();
+				//destroyline += latestLine.EnbleGravity;	
 			}
 
 
@@ -67,22 +103,55 @@ public class AnchorManger : MonoBehaviour {
 			}
 			//Transform point = transform.Find ("");
 			Vector3 ppos = new Vector3(point.position.x,point.position.y,point.position.z);
-
+			/*if (latestLine != null && currenttime <= 0) {
+				GameObject obj = Instantiate (line.gameObject, ppos, Quaternion.identity, null);
+				//destroyline += latestLine.EnbleGravity;
+				latestLine.gameObject2 = obj.transform;
+				latestLine = obj.GetComponent<ConnectLine> ();
+				currenttime = -0.2f;
+			}*/
 			if (currentobject != null) {
 				currentobject.transform.position = ppos;
 			}
-
+			audio.volume += Time.deltaTime;
+			return;
 			//lastanchorrotation = anchor.transform.rotation;
 		}
-		if (Input.GetMouseButtonUp (0)) {
+		if (Input.GetMouseButtonUp (0) && currentobject != null) {
 			ParticleSystem.EmissionModule pEmission = currentobject.GetComponent<ParticleSystem> ().emission;
-			pEmission.enabled = false;
+				pEmission.enabled = false;
 		//	pMain.loop = false;
-		//	Destroy (pParticle);
+			//Destroy (currentobject);
 			currentobject = null;
 		}
-	
+		currenttime -= Time.deltaTime;
 
+		if( Input.GetMouseButton(1)) {
+			if (destroyline != null) {
+				destroyline.Invoke ();
+			}
+		}
+		audio.volume -= Time.deltaTime;
+		Vector3 acceleration = Input.acceleration;
+		lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
+		Vector3 deltaAcceleration = acceleration - lowPassValue;
 
+		if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
+		{
+			// Perform your "shaking actions" here. If necessary, add suitable
+			// guards in the if check above to avoid redundant handling during
+			// the same shake (e.g. a minimum refractory period).
+			Debug.Log("Shake event detected at time "+Time.time);
+		//	Invoke ("Destroline", 2);
+		}
 	}
+
+	void Destroline()
+	{
+		if (destroyline != null) {
+			destroyline.Invoke ();
+		}
+	}
+
+
 }
---
+++ Assets/Scripts/ConnectLine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectLine : MonoBehaviour {
+
+	public Transform gameObject1;          // Reference to the first GameObject
+	public Transform gameObject2;          // Reference to the second GameObject
+
+	public LineRenderer line; 
+	public Rigidbody rg;
+
+	// Use this for initialization
+	void Start () {
+		gameObject1 = this.transform;
+		gameObject2 = this.transform;
+		AnchorManger.instance.destroyline += EnableGravity;
+		//line.SetPosition(0, gameObject1.transform.position);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Check if the GameObjects are not null
+		if (gameObject1 != null && gameObject2 != null)
+		{
+			// Update position of the two vertex of the Line Renderer
+			line.SetPosition(0, gameObject1.transform.position);
+			line.SetPosition(1, gameObject2.transform.position);
+		}
+	}
+
+	 void EnableGravity()
+	{
+		rg.isKinematic = false;
+		AnchorManger.instance.destroyline -= EnableGravity;
+		Destroy (gameObject, 2);
+	}
+}
